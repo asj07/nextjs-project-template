aa81bf7bfbecf7fad9f9a40fad0bc3e9948483a2
Abishek S J <159030129+asj07@users.noreply.github.com>
2025-06-27 20:53:34 +0530

    Add .next/static/chunks/src_app_admin_dashboard_page_tsx_617d71eb._.js 1751037813326

---
+++ .next/static/chunks/src_app_admin_dashboard_page_tsx_617d71eb._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_71df84e5._.js",
+  "static/chunks/src_dd2d288c._.js"
+],
+    source: "dynamic"
+});
